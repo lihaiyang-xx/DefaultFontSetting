140e7000339df619b7ea63708f0777d82cd2b098
Ocean <762923309@qq.com>
2019-09-06 11:54:03 +0800

    Set Default Text Font

---
+++ Assets/SetDefaultFont/Editor/DefaultFontSettingLaunch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[InitializeOnLoad]
+public class DefaultFontSettingLaunch : Editor
+{
+    public static Font defaultFont;
+    static DefaultFontSettingLaunch()
+    {
+        UpdateFont();
+
+        EditorApplication.hierarchyWindowChanged += OnHierarchyWindowChanged;
+
+        Debug.Log("default font:" + defaultFont);
+    }
+
+    
+	static void OnHierarchyWindowChanged()
+    {
+        if(!defaultFont)
+            return;
+        GameObject selectGameObject = Selection.activeGameObject;
+        if (selectGameObject)
+        {
+            Text[] texts = selectGameObject.GetComponentsInChildren<Text>();
+            foreach (var text in texts)
+            {
+                text.font = defaultFont;
+            }
+        }
+    }
+
+    public static void UpdateFont()
+    {
+        defaultFont = GetDefaultFont();
+    }
+
+
+    public static Font GetDefaultFont()
+    {
+        string guid = EditorPrefs.GetString("fontGUID");
+        if (!string.IsNullOrEmpty(guid))
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            Font font = AssetDatabase.LoadAssetAtPath<Font>(path);
+            return font;
+        }
+
+        return null;
+    }
+}
---
+++ Assets/SetDefaultFont/Editor/DefaultFontSettingWindow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DefaultFontSettingWindow : EditorWindow
+{
+    private Font font;
+    [MenuItem("DefaultFont/Setting")]
+    static void InitWindow()
+    {
+        DefaultFontSettingWindow window = EditorWindow.GetWindow<DefaultFontSettingWindow>();
+        //window.Show();
+    }
+
+    void Reset()
+    {
+        font = DefaultFontSettingLaunch.GetDefaultFont();
+    }
+
+
+    void OnGUI()
+    {
+        font = EditorGUILayout.ObjectField("Default Font", font, typeof(Font), false) as Font;
+        GUILayout.Space(100);
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Apply",GUILayout.Width(100)))
+        {
+            if (font)
+            {
+                string path = AssetDatabase.GetAssetPath(font);
+                string guid = AssetDatabase.AssetPathToGUID(path);
+                EditorPrefs.SetString("fontGUID", guid);
+                Debug.Log("set default font : " + font.name);
+            }
+            else
+            {
+                EditorPrefs.DeleteKey("fontGUID");
+                Debug.Log("clear custom default font ,will use unity default font arial");
+            }
+
+            DefaultFontSettingLaunch.UpdateFont();
+        }
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+    }
+
+}
